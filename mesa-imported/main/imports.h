740d0ed283bffc38fa873a2ca72691156dcb431b
fincs <fincs.alt1@gmail.com>
2019-01-13 12:58:13 +0100

    Disable strtok_r -> strtok_s hack on Windows

--- mesa-imported/main/imports.h
+++ mesa-imported/main/imports.h
@@ -324,9 +324,11 @@ extern int
 _mesa_vsnprintf(char *str, size_t size, const char *fmt, va_list arg);
 
 
+/* fincs-edit
 #if defined(_WIN32) && !defined(strtok_r)
 #define strtok_r strtok_s
 #endif
+*/
 
 #ifdef __cplusplus
 }
