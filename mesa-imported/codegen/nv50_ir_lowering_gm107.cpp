devkitPro/uam

commit 7f1aaabf694bbd99235474b5b08a6b718a3f2bd8

Add native support for non-bindless image operations

mesa-imported/codegen/nv50_ir_lowering_gm107.cpp
@@ -270,17 +270,19 @@ GM107LoweringPass::handleSUQ(TexInstruction *suq)
    const int slot = suq->tex.r;
    const int mask = suq->tex.mask;
 
+   /* fincs-edit: Added support for proper non-bindless access
    if (suq->tex.bindless)
       handle = ind;
    else
       handle = loadTexHandle(ind, slot + 32);
+   */
 
-   suq->tex.r = 0xff;
+   suq->tex.r = suq->tex.bindless ? 0xff : (prog->driver->io.texBindBase/4 + slot + 32); // fincs-edit
    suq->tex.s = 0x1f;
 
    suq->setIndirectR(NULL);
-   suq->setSrc(0, handle);
-   suq->tex.rIndirectSrc = 0;
+   suq->setSrc(0, suq->tex.bindless ? ind : bld.loadImm(NULL, 0)); // fincs-edit
+   suq->tex.rIndirectSrc = suq->tex.bindless ? 0 : -1; // fincs-edit
    suq->setSrc(1, bld.loadImm(NULL, 0));
    suq->tex.query = TXQ_DIMS;
    suq->op = OP_TXQ;
@@ -320,11 +322,11 @@ GM107LoweringPass::handleSUQ(TexInstruction *suq)
 
       if (mask & 0x1)
          bld.mkOp2(OP_SHR, TYPE_U32, suq->getDef(0), suq->getDef(0),
-                   loadMsAdjInfo32(suq->tex.target, 0, slot, handle, true)); // fincs-edit: use bindless handle directly instead of driver UBO
+                   loadMsAdjInfo32(suq->tex.target, 0, slot, ind, suq->tex.bindless));
       if (mask & 0x2) {
          int d = util_bitcount(mask & 0x1);
          bld.mkOp2(OP_SHR, TYPE_U32, suq->getDef(d), suq->getDef(d),
-                   loadMsAdjInfo32(suq->tex.target, 1, slot, handle, true)); // fincs-edit: use bindless handle directly instead of driver UBO
+                   loadMsAdjInfo32(suq->tex.target, 1, slot, ind, suq->tex.bindless));
       }
    }
 

mesa-imported/codegen/nv50_ir_lowering_nvc0.cpp
@@ -1759,10 +1759,10 @@ NVC0LoweringPass::loadSuInfo32(Value *ptr, int slot, uint32_t off, bool bindless
 Value *
 NVC0LoweringPass::loadMsAdjInfo32(TexInstruction::Target target, uint32_t index, int slot, Value *ind, bool bindless)
 {
-   if (!bindless || targ->getChipset() < NVISA_GM107_CHIPSET)
+   if (/*!bindless ||*/ targ->getChipset() < NVISA_GM107_CHIPSET) // fincs-edit
       return loadSuInfo32(ind, slot, NVC0_SU_INFO_MS(index), bindless);
 
-   assert(bindless);
+   //assert(bindless); // fincs-edit
 
    Value *samples = bld.getSSA();
    // this shouldn't be lowered because it's being inserted before the current instruction
@@ -1770,11 +1770,11 @@ NVC0LoweringPass::loadMsAdjInfo32(TexInstruction::Target target, uint32_t index,
    tex->tex.target = target;
    tex->tex.query = TXQ_TYPE;
    tex->tex.mask = 0x4;
-   tex->tex.r = 0xff;
+   tex->tex.r = bindless ? 0xff : (prog->driver->io.texBindBase/4 + slot + 32); // fincs-edit
    tex->tex.s = 0x1f;
-   tex->tex.rIndirectSrc = 0;
+   tex->tex.rIndirectSrc = bindless ? 0 : -1; // fincs-edit
    tex->setDef(0, samples);
-   tex->setSrc(0, ind);
+   tex->setSrc(0, bindless ? ind : bld.loadImm(NULL, 0)); // fincs-edit
    tex->setSrc(1, bld.loadImm(NULL, 0));
    bld.insert(tex);
 
@@ -1880,18 +1880,8 @@ NVC0LoweringPass::adjustCoordinatesMS(TexInstruction *tex)
    Value *tx = bld.getSSA(), *ty = bld.getSSA(), *ts = bld.getSSA();
    Value *ind = tex->getIndirectR();
 
-   // fincs-edit block start
-   Value *handle = ind;
-   bool load_bindless = tex->tex.bindless;
-   if (targ->getChipset() >= NVISA_GM107_CHIPSET) {
-      // Prefer bindless access as opposed to loading information from driver UBO
-      handle = loadTexHandle(ind, slot + 32);
-      load_bindless = true;
-   }
-
-   Value *ms_x = loadMsAdjInfo32(tex->tex.target, 0, slot, handle, load_bindless);
-   Value *ms_y = loadMsAdjInfo32(tex->tex.target, 1, slot, handle, load_bindless);
-   // fincs-edit block end
+   Value *ms_x = loadMsAdjInfo32(tex->tex.target, 0, slot, ind, tex->tex.bindless);
+   Value *ms_y = loadMsAdjInfo32(tex->tex.target, 1, slot, ind, tex->tex.bindless);
 
    bld.mkOp2(OP_SHL, TYPE_U32, tx, x, ms_x);
    bld.mkOp2(OP_SHL, TYPE_U32, ty, y, ms_y);
@@ -2445,7 +2435,7 @@ NVC0LoweringPass::processSurfaceCoordsGM107(TexInstruction *su)
    if (su->tex.bindless)
       handle = ind;
    else
-      handle = loadTexHandle(ind, slot + 32);
+      handle = bld.mkImm(prog->driver->io.texBindBase/4 + slot + 32); // fincs-edit
    su->setSrc(arg + pos, handle);
 
 #if 0 // fincs-edit: Removing access checks for bound textures too.

commit b8df713edfe46a84743ddbce6b8960d92530b606

Fix non-bindless indirect surface operations (broken by 7f1aaabf6)

mesa-imported/codegen/nv50_ir_lowering_gm107.cpp
@@ -266,7 +266,7 @@ bool
 GM107LoweringPass::handleSUQ(TexInstruction *suq)
 {
    Value *ind = suq->getIndirectR();
-   Value *handle;
+   //Value *handle; // fincs-edit
    const int slot = suq->tex.r;
    const int mask = suq->tex.mask;
 
@@ -276,13 +276,18 @@ GM107LoweringPass::handleSUQ(TexInstruction *suq)
    else
       handle = loadTexHandle(ind, slot + 32);
    */
+   bool bindless = suq->tex.bindless;
+   if (!bindless && ind) {
+      bindless = true;
+      ind = loadTexHandle(ind, slot + 32);
+   }
 
-   suq->tex.r = suq->tex.bindless ? 0xff : (prog->driver->io.texBindBase/4 + slot + 32); // fincs-edit
+   suq->tex.r = bindless ? 0xff : (prog->driver->io.texBindBase/4 + slot + 32); // fincs-edit
    suq->tex.s = 0x1f;
 
    suq->setIndirectR(NULL);
-   suq->setSrc(0, suq->tex.bindless ? ind : bld.loadImm(NULL, 0)); // fincs-edit
-   suq->tex.rIndirectSrc = suq->tex.bindless ? 0 : -1; // fincs-edit
+   suq->setSrc(0, bindless ? ind : bld.loadImm(NULL, 0)); // fincs-edit
+   suq->tex.rIndirectSrc = bindless ? 0 : -1; // fincs-edit
    suq->setSrc(1, bld.loadImm(NULL, 0));
    suq->tex.query = TXQ_DIMS;
    suq->op = OP_TXQ;
@@ -322,11 +327,11 @@ GM107LoweringPass::handleSUQ(TexInstruction *suq)
 
       if (mask & 0x1)
          bld.mkOp2(OP_SHR, TYPE_U32, suq->getDef(0), suq->getDef(0),
-                   loadMsAdjInfo32(suq->tex.target, 0, slot, ind, suq->tex.bindless));
+                   loadMsAdjInfo32(suq->tex.target, 0, slot, ind, bindless));
       if (mask & 0x2) {
          int d = util_bitcount(mask & 0x1);
          bld.mkOp2(OP_SHR, TYPE_U32, suq->getDef(d), suq->getDef(d),
-                   loadMsAdjInfo32(suq->tex.target, 1, slot, ind, suq->tex.bindless));
+                   loadMsAdjInfo32(suq->tex.target, 1, slot, ind, bindless));
       }
    }
 

mesa-imported/codegen/nv50_ir_lowering_nvc0.cpp
@@ -1875,6 +1875,10 @@ NVC0LoweringPass::loadMsAdjInfo32(TexInstruction::Target target, uint32_t index,
       return loadSuInfo32(ind, slot, NVC0_SU_INFO_MS(index), bindless);
 
    //assert(bindless); // fincs-edit
+   if (!bindless && ind) {
+      bindless = true;
+      ind = loadTexHandle(ind, slot + 32);
+   }
 
    Value *samples = bld.getSSA();
    // this shouldn't be lowered because it's being inserted before the current instruction
@@ -2546,7 +2550,9 @@ NVC0LoweringPass::processSurfaceCoordsGM107(TexInstruction *su)
    }
    if (su->tex.bindless)
       handle = ind;
-   else
+   else if (ind) // fincs-edit
+      handle = loadTexHandle(ind, slot + 32);
+   else // fincs-edit
       handle = bld.mkImm(prog->driver->io.texBindBase/4 + slot + 32); // fincs-edit
    su->setSrc(arg + pos, handle);
 